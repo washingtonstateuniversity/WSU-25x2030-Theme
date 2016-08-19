b0be6269c0504e1b065d7853e8e0c4ca784d2d8f
Phil Cable <pcable@wsu.edu>
2016-08-19 12:20:48 -0700

    Introduce CSS and Javascript for the top section parallax effect

--- functions.php
+++ functions.php
@@ -32,7 +32,6 @@ class WSU_25_by_2030_Theme {
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'wsu-25-by-2030', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ) );
 	}
-
 }
 
 add_action( 'after_setup_theme', 'WSU_25_by_2030_Theme' );
