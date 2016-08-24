8fd3b9371b02231091af5c6d3bc36ac8714e980f
Jeremy Felt <jeremy.felt@gmail.com>
2016-08-24 16:41:26 -0700

    Add a script version to aid in cache busting

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 
 class WSU_25_by_2030_Theme {
+
+	/**
+	 * @var string String used for busting cache on scripts.
+	 */
+	var $script_version = '0001';
+
 	/**
 	 * @var WSU_25_by_2030_Theme
 	 */
@@ -44,10 +50,10 @@ class WSU_25_by_2030_Theme {
 	 * Enqueue the scripts used in the theme.
 	 */
 	public function enqueue_scripts() {
-		wp_enqueue_script( 'wsu-25-by-2030', get_stylesheet_directory_uri() . '/js/scripts.min.js', array( 'jquery', 'wsu-25-by-2030-doormat' ), false, true );
+		wp_enqueue_script( 'wsu-25-by-2030', get_stylesheet_directory_uri() . '/js/scripts.min.js', array( 'jquery', 'wsu-25-by-2030-doormat' ), $this->script_version, true );
 		wp_enqueue_script( 'wsu-25-by-2030-typekit', 'https://use.typekit.net/roi0hte.js', array(), false );
 		wp_add_inline_script( 'wsu-25-by-2030-typekit', 'try{Typekit.load();}catch(e){};' );
-		wp_enqueue_script( 'wsu-25-by-2030-doormat', get_stylesheet_directory_uri() . '/js/doormat.min.js', array( 'jquery' ), false, true );
+		wp_enqueue_script( 'wsu-25-by-2030-doormat', get_stylesheet_directory_uri() . '/js/doormat.min.js', array( 'jquery' ), $this->script_version, true );
 		wp_add_inline_script( 'wsu-25-by-2030-doormat', 'var drive_doormat = new Doormat({ debounce: 150, snapping: { travel: false, viewport: false } });' );
 		wp_add_inline_script( 'wsu-25-by-2030-doormat', "jQuery('.story').wrapAll('<div id=\"stories\" class=\"section-wrapper panel gray-dark-back white-text\" />');", 'before' );
 	}
