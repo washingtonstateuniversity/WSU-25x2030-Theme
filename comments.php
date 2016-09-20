ac76683b36908163cc86110d54c23f856984d132
Phil Cable <pcable@wsu.edu>
2016-09-20 10:57:08 -0700

    Include `$post_id` parameter in `comment_form` call so that comments submit to the correct post Remove `have_comments` check from around the comment list output

--- comments.php
+++ comments.php
@@ -17,59 +17,61 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php
+	$front_page_id = get_option( 'page_on_front' );
+
 	comment_form( array(
 		'fields' => array(
 			'author' => '<p class="comment-form-author"><label for="author">Name <span class="optional">(Optional)</span></label> <input id="author" name="author" value="" maxlength="245" aria-required="true" required="required" type="text"></p>',
 			'email' => '<p class="comment-form-email"><label for="email">Email Address <span class="optional">(Optional)</span></label> <input id="email" name="email" value="" maxlength="100" aria-describedby="email-notes" aria-required="true" required="required" type="email"></p>',
+			),
+			'comment_field' => '<p class="comment-form-comment"><label for="comment">Comments</label> <textarea id="comment" name="comment" maxlength="65525" aria-required="true" required="required"></textarea></p>',
+			'comment_notes_before' => '',
+			'class_form' => 'drive-comment-form',
+			'title_reply' => '',
+			'title_reply_to' => '',
+			'title_reply_before' => '',
+			'title_reply_after' => '',
+			'cancel_reply_before' => '',
+			'cancel_reply_after' => '',
+			'cancel_reply_link' => '',
+			'label_submit' => 'Submit feedback',
+			'format' => 'html5',
 		),
-		'comment_field' => '<p class="comment-form-comment"><label for="comment">Comments</label> <textarea id="comment" name="comment" maxlength="65525" aria-required="true" required="required"></textarea></p>',
-		'comment_notes_before' => '',
-		'class_form' => 'drive-comment-form',
-		'title_reply' => '',
-		'title_reply_to' => '',
-		'title_reply_before' => '',
-		'title_reply_after' => '',
-		'cancel_reply_before' => '',
-		'cancel_reply_after' => '',
-		'cancel_reply_link' => '',
-		'label_submit' => 'Submit feedback',
-		'format' => 'html5',
+		$front_page_id
+	);
+
+	$comments = get_comments( array(
+		'post_id' => $front_page_id,
+		'status' => 'approve',
 	) );
 
-	if ( have_comments() ) {
-		$comments = get_comments( array(
-			'post_id' => get_option( 'page_on_front' ),
-			'status' => 'approve',
-		) );
+	?><div class="comment-list"><?php
 
-		?><div class="comment-list"><?php
+	wp_list_comments( array(
+		'max_depth' => 1,
+		'style' => 'div',
+		'type' => 'comment',
+		'avatar_size' => 0,
+		'format' => 'html5',
+		'reverse_top_level' => false,
+	), $comments );
 
-		wp_list_comments( array(
-			'max_depth' => 1,
-			'style' => 'div',
-			'type' => 'comment',
-			'avatar_size' => 0,
-			'format' => 'html5',
-			'reverse_top_level' => false,
-		), $comments );
+	?>
+	</div>
+	<?php
 
-		?>
-		</div>
-		<?php
+	if ( get_comment_pages_count() > 1 ) {
 
-		if ( get_comment_pages_count() > 1 ) {
+		?><div class="comment-nav"><?php
 
-			?><div class="comment-nav"><?php
+		$comment_navigation_args = array(
+			'prev_text' => 'Newer comments',
+			'next_text' => 'Older comments',
+		);
 
-			$comment_navigation_args = array(
-				'prev_text' => 'Newer comments',
-				'next_text' => 'Older comments',
-			);
+		the_comments_navigation( $comment_navigation_args );
 
-			the_comments_navigation( $comment_navigation_args );
-
-			?></div><?php
-		}
+		?></div><?php
 	}
 	?>
 
