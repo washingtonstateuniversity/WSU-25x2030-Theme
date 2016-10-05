e1fd53645b06a014b7238fc89592088f982479ae
Phil Cable <pcable@wsu.edu>
2016-10-05 10:16:19 -0700

    Improve handling of comments that consist of multiple paragraphs
    This addition to the `comment_text` filter wraps the remaining
    text of a comment that spans multiple paragraphs in a slightly
    more reliable fashion.

--- functions.php
+++ functions.php
@@ -283,7 +283,19 @@ class WSU_25_by_2030_Theme {
 
 				if ( '' !== $excerpt ) {
 					$comment_remainder = substr( $comment_text, strlen( $excerpt ), $length );
-					$comment_text = $excerpt . '<span class="ellipsis">&hellip;</span><span class="comment-remainder">' . $comment_remainder . '</span><br /><a href="#" class="remainder-toggle">&raquo; Show more</a>';
+					$toggle_link = '<p><a href="#" class="remainder-toggle">&raquo; Show more</a></p>';
+
+					if ( strpos( $comment_remainder, "\n\n" ) !== FALSE ) {
+						$paragraphs = explode( "\n\n", $comment_remainder );
+						$new_remainder = '<span class="ellipsis">&hellip;</span><span class="comment-remainder">' . $paragraphs[0]. '</span>';
+
+						unset($paragraphs[0]);
+
+						$new_remainder .= '<div class="comment-remainder">' . "\n\n" . implode( "\n\n", $paragraphs ) . "\n\n" . '</div>';
+						$comment_text = $excerpt . $new_remainder . $toggle_link;
+					} else {
+						$comment_text = $excerpt . '<span class="ellipsis">&hellip;</span><span class="comment-remainder">' . $comment_remainder . '</span>' . "\n" . $toggle_link;
+					}
 				}
 			}
 		}
