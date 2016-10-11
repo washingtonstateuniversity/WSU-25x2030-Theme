06ea954d66ee28cc1bf95a6770d7733c4ba5cd4f
Phil Cable <pcable@wsu.edu>
2016-10-11 15:58:09 -0700

    Add initial scroll handling JavaScript for the filtering feature

--- style-guide/js/evidence.js
+++ style-guide/js/evidence.js
@@ -2,6 +2,10 @@
 
 	//'use strict';
 
+	var $filters = $('#story-filters'),
+		$options = $('#filter-options'),
+		offset = ($('body').hasClass('admin-bar')) ? 82 : 50;
+
 	/**
 	 * Look for any sections with background images stored as data attributes
 	 * and convert the data attribute into inline CSS for that section.
@@ -19,8 +23,59 @@
 		});
 	};
 
+	/**
+	 * Apply `fixed` class to the filter drop-down if it has been scrolled to/past.
+	 */
+	fix_filters = function() {
+		var $options_offset = $options.offset().top - offset;
+
+		$(document).on('scroll', function () {
+			if ($(window).scrollTop() >= $options_offset) {
+				$options.addClass('fixed');
+			} else {
+				$options.removeClass('fixed');
+			}
+		});
+	};
+
+	/**
+	 * Scroll to the filter drop-down when an option is selected.
+	 */
+	scroll_to_filter = function() {
+		var filters_top = Math.round($filters.offset().top - offset);
+
+		if ($(window).scrollTop() !== filters_top) {
+			$('html, body').animate({
+				scrollTop: filters_top
+			}, 750);
+		}
+	}
+
+	/**
+	 * Update the header text when an option is selected from the filter drop-down.
+	 */
+	update_title = function( name, value ) {
+		var $title_heading = $('.topic-title').find('h3');
+
+		if ( value ) {
+			$title_heading.html(name);
+		} else {
+			$title_heading.html('&nbsp;');
+		}
+	}
+
 	$(document).ready( function() {
 		process_column_backgrounds();
+		fix_filters();
+	});
+
+	$options.on( 'change', function() {
+		var option = $(this).find('option:selected'),
+			name = option.text(),
+			value = option.val();
+
+		scroll_to_filter();
+		update_title( name, value );
 	});
 
 }(jQuery, window));
