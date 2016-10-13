f17e2e6e86ebf14ee0521b787025935980d48dac
Phil Cable <pcable@wsu.edu>
2016-10-13 10:43:27 -0700

    Update JavaScript with history state and AJAX call functions
    This commit also refactors how the functions are defined and makes
    other syntactical changes throughout for consistency. Apologies in
    advance for the confusing diff.

--- style-guide/js/evidence.js
+++ style-guide/js/evidence.js
@@ -1,24 +1,27 @@
-(function ($, window) {
+(function ($, window, document, evidence) {
 
-	//'use strict';
+	'use strict';
 
 	var $filters = $('#story-filters'),
 		$options = $('#filter-options'),
-		offset = ($('body').hasClass('admin-bar')) ? 82 : 50;
+		offset = ($('body').hasClass('admin-bar')) ? 82 : 50,
+		fetching = false,
+		all_stories = false,
+		$story_container = $('.evidence-stories-container');
 
 	/**
 	 * Look for any sections with background images stored as data attributes
 	 * and convert the data attribute into inline CSS for that section.
 	 */
-	process_column_backgrounds = function() {
-		$('.column.one').each( function() {
+	process_column_backgrounds = function () {
+		$('.column.one').each(function () {
 			var background_image = $(this).data('background'),
 				mobile_background_image = $(this).data('background-mobile');
 
-			if ( 990 > $(window).width() && mobile_background_image ) {
-				$(this).css('background-image', 'url(' + mobile_background_image + ')' );
-			} else if ( background_image ) {
-				$(this).css('background-image', 'url(' + background_image + ')' );
+			if (990 > $(window).width() && mobile_background_image) {
+				$(this).css('background-image', 'url(' + mobile_background_image + ')');
+			} else if (background_image) {
+				$(this).css('background-image', 'url(' + background_image + ')');
 			}
 		});
 	};
@@ -26,7 +29,7 @@
 	/**
 	 * Apply `fixed` class to the filter drop-down if it has been scrolled to/past.
 	 */
-	fix_filters = function() {
+	fix_filters = function () {
 		var $options_offset = $options.offset().top - offset;
 
 		$(document).on('scroll', function () {
@@ -39,9 +42,83 @@
 	};
 
 	/**
-	 * Scroll to the filter drop-down when an option is selected.
+	 * Check if the footer is in the viewport.
 	 */
-	scroll_to_filter = function() {
+	footer_is_visible = function () {
+		var footer = $('.site-footer');
+
+		if (footer instanceof jQuery) {
+			footer = footer[0];
+		}
+
+		var rect = footer.getBoundingClientRect();
+
+		return rect.bottom > 0 &&
+			rect.right > 0 &&
+			rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+			rect.top < (window.innerHeight || document.documentElement.clientHeight);
+	};
+
+	/**
+	 * Make an AJAX call and display the response.
+	 */
+	fetch_and_display_stories = function (data, type) {
+		$.ajax({
+			type: 'post',
+			url: evidence.ajax_url,
+			data: data,
+			beforeSend: function () {
+				fetching = true;
+				if ('scrolled' === type) {
+					$story_container.append('<div class="loading"></div>');
+				} else if ('filtered' === type) {
+					$story_container.find('.section-wrapper').hide(400);
+				}
+			}
+		}).done(function (response) {
+			if ('scrolled' === type) {
+				$('.loading').remove();
+				$story_container.append($.parseJSON(response));
+				process_column_backgrounds();
+				$story_container.data('page', data.page + 1);
+			} else if ('filtered' === type) {
+				$story_container.html($.parseJSON(response));
+				process_column_backgrounds();
+				$story_container.data('page', 1);
+			}
+			$('.new').show(400);
+		}).always(function () {
+			fetching = false;
+		});
+	};
+
+	/**
+	 * Load more posts when the footer is reached.
+	 */
+	infinite_scroll = function () {
+		window.addEventListener('scroll', function () {
+			if (fetching ||
+					all_stories ||
+					!footer_is_visible() ||
+					$story_container.data('page') === $story_container.data('total-pages') ||
+					'' !== $('#filter-options').find('option:selected').val()) {
+				return;
+			}
+
+			var data = {
+					action: 'evidence_stories',
+					nonce: evidence.nonce,
+					page: $story_container.data('page')
+				};
+
+			fetch_and_display_stories(data, 'scrolled');
+		}, 500);
+	};
+
+	/**
+	 * Scroll to the filter wrapper when an option is selected.
+	 */
+	scroll_to_filter = function () {
 		var filters_top = Math.round($filters.offset().top - offset);
 
 		if ($(window).scrollTop() !== filters_top) {
@@ -49,33 +126,49 @@
 				scrollTop: filters_top
 			}, 750);
 		}
-	}
+	};
 
 	/**
-	 * Update the header text when an option is selected from the filter drop-down.
+	 * Update the header text and document title when an option is selected from the filter drop-down.
 	 */
-	update_title = function( name, value ) {
-		var $title_heading = $('.topic-title').find('h3');
+	update_title = function (name, value) {
+		var heading = value ? name : ' ',
+			doc_title = value ? name + ' | ' + evidence.default_title : evidence.default_title;
 
-		if ( value ) {
-			$title_heading.html(name);
-		} else {
-			$title_heading.html('&nbsp;');
-		}
-	}
+		$('.topic-title').find('h3').html(heading);
 
-	$(document).ready( function() {
+		document.title = doc_title;
+	};
+
+	/**
+	 * Update the history state.
+	 */
+	update_history = function (value) {
+		var url = value ? evidence.default_url + 'category/' + value : evidence.default_url;
+
+		history.pushState(null, null, url);
+	};
+
+	$(document).ready(function () {
 		process_column_backgrounds();
 		fix_filters();
+		infinite_scroll();
 	});
 
-	$options.on( 'change', function() {
+	$options.on('change', function () {
 		var option = $(this).find('option:selected'),
 			name = option.text(),
-			value = option.val();
+			value = option.val(),
+			data = {
+				action: 'evidence_stories',
+				nonce: evidence.nonce,
+				category: value
+			};
 
 		scroll_to_filter();
-		update_title( name, value );
+		update_title(name, value);
+		update_history(value);
+		fetch_and_display_stories(data, 'filtered');
 	});
 
-}(jQuery, window));
+}(jQuery, window, document, evidence));
