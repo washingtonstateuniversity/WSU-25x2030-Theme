4e21f1a0c04f4a9efa2ca5e826ad6c4e7769843e
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-27 13:22:14 -0700

    Bump version 0.3.0

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ class WSU_25_by_2030_Theme {
 	/**
 	 * @var string String used for busting cache on scripts.
 	 */
-	var $script_version = '0.2.0';
+	var $script_version = '0.3.0';
 
 	/**
 	 * @var int Comments to display per page.
